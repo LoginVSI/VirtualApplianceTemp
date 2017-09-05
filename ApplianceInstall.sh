13b9689999ecf621b8850dd9f8e0eeeb561bf726
h.hofs <h.hofs@loginvsi.com>
2017-09-05 11:24:27 +0200

    updated cert generation

--- ApplianceInstall.sh
+++ ApplianceInstall.sh
@@ -54,6 +54,7 @@ cp -f $SCRIPT_PATH/loginvsid.service /etc/systemd/system/
 cp -f $SCRIPT_PATH/firstrun /loginvsi/
 cp -f $SCRIPT_PATH/.env /loginvsi/
 cp -f $SCRIPT_PATH/sshd_config /etc/ssh/
+hostname "loginvsi-ng"
 
 echo "#!/bin/bash" > /home/administrator/.bash_profile
 echo "if [ ! -f '/loginvsi/first_run.chk' ]; then"  >> /home/administrator/.bash_profile
