701ef289f986ab5ec6178fb2393b31e48058fd26
h.hofs <h.hofs@loginvsi.com>
2017-10-11 12:39:57 +0200

    removed httpd

--- ApplianceInstall.sh
+++ ApplianceInstall.sh
@@ -14,8 +14,7 @@ apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
-    software-properties-common \
-    httpd
+    software-properties-common    
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
