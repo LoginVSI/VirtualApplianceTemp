05171fa80d79f6b783305ca44b2587bc4d9def32
h.hofs <h.hofs@loginvsi.com>
2017-10-13 12:25:45 +0200

    bugs bugs bugs

--- ApplianceInstall.sh
+++ ApplianceInstall.sh
@@ -21,7 +21,7 @@ add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-apt-get update -f
+apt-get update
 apt-get install -y docker-ce
 
 # install docker-compose
