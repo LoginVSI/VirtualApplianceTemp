667da07f5e2121f7e4679ff1f48c8cd1d029a0f6
h.hofs <h.hofs@loginvsi.com>
2018-09-06 08:55:23 +0200

    added daemon.json for docker log rotation settings, preventing the logs to become too big

--- newonly/ApplianceInstall.sh
+++ newonly/ApplianceInstall.sh
@@ -99,7 +99,7 @@ cp -f $SCRIPT_PATH/../loginvsid /usr/bin/
 cp -f $SCRIPT_PATH/../loginvsid.service /etc/systemd/system/
 cp -f $SCRIPT_PATH/../docker-cleanup.service /etc/systemd/system/
 cp -f $SCRIPT_PATH/firstrun /loginvsi/
-#cp -f $SCRIPT_PATH/sshd_config /etc/ssh/
+cp -f $SCRIPT_PATH/../daemon.json /etc/docker
 
 
 
